ff9c00dad937330beb94099449b6b1af995d9e3e
BeatrizCruz <55484817+BeatrizCruz@users.noreply.github.com>
2019-10-23 23:53:35 +0100

    My_project

---
+++ My_project2.py
@@ -0,0 +1,384 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 13 13:44:55 2019
+
+@author: aSUS
+"""
+import pandas as pd
+import sqlite3
+my_path = 'C:/Users/aSUS/Documents/IMS/Master Data Science and Advanced Analytics with major in BA/Data mining/Projeto/insurance.db'
+#dbname = "datamining.db"
+
+# connect to the database
+conn = sqlite3.connect(my_path)
+#cursor = conn.cursor()
+conn.row_factory=sqlite3.Row
+
+#tables in the data base:
+query = "select name from sqlite_master where type='table'"
+df2 = pd.read_sql_query(query,conn)
+
+#Columns in each table of the data base:
+query2="select sql from sqlite_master where tbl_name='LOB' and type='table'"
+print(pd.read_sql_query(query2,conn))
+
+cur.execute('select * from LOB')
+col_name_list=[tuple[0] for tuple in cur.description]
+
+query="""
+select * from lob limit(10);
+"""
+
+query="""
+select * from engage limit(10);
+"""
+ 
+my_table= cursor.execute(query).fetchall()
+cursor.execute("select name from sqlite_master where type='table'")
+print(cursor.fetchall())
+
+#Diretorias:
+file='C:/Users/aSUS/Documents/IMS/Master Data Science and Advanced Analytics with major in BA\Data mining/Projeto/A2Z Insurance.csv'
+
+#import csv file:
+import pandas as pd
+dfOriginal=pd.read_csv(file)
+dfOriginal.head()
+
+#Rename column
+colNames=list(dfOriginal.columns)
+colNames
+dfOriginal=dfOriginal.rename(columns={"Brithday Year": "birthday",
+                                        "Customer Identity":"id",
+                                        "First Policy´s Year":"firstPolicy",
+                                        "Educational Degree":"education",
+                                        "Gross Monthly Salary":"salary",
+                                        "Geographic Living Area":"livingArea",
+                                        "Has Children (Y=1)":"children",
+                                        "Customer Monetary Value":"cmv",
+                                        "Claims Rate":"claims",
+                                        "Premiums in LOB: Motor":"lobMotor",
+                                        "Premiums in LOB: Household":"lobHousehold",
+                                        "Premiums in LOB: Health":"lobHealth",
+                                        "Premiums in LOB:  Life":"lobLife",
+                                        "Premiums in LOB: Work Compensations":"lobWork"})
+#Check null values
+dfOriginal.isnull().sum()
+
+#Check categories in livingArea variable
+dfOriginal["livingArea"].unique()
+#-----------------DATA TYPES------------------#
+
+dfOriginal.loc[:,"firstPolicy"]=pd.to_numeric(dfOriginal.loc[:,"firstPolicy"],errors='coerce')
+dfOriginal.loc[3,"firstPolicy"].dtype
+dfOriginal.iloc[]=dfOriginal.iloc[:,2].astype('int64')
+dfOriginal.iloc[:,5]=dfOriginal.iloc[:,5].astype('int64')
+dfOriginal.iloc[:,6]=dfOriginal.iloc[:,6].astype('int64')
+
+#------------------UNDERSTAND VARIABLES-------------------#
+
+#Pivot tables
+#Salary mean having in mind children and livingArea 
+pd.pivot_table(dfOriginal,
+               values='salary',
+               index='livingArea',
+               columns=['children'],
+               aggfunc='mean',
+               margins=True)
+#number of individuals having in mind education and livingArea 
+pd.pivot_table(dfOriginal,
+               values='salary',
+               index='livingArea',
+               columns=['education'],
+               aggfunc='count',margins=True) #ver em proporção
+
+#Variables univariate Distributions:
+sb.distplot(dfOriginal['claims'],kde=False)
+plt.show()
+
+#Plots:
+from matplotlib import pyplot as plt
+import seaborn as sb
+
+#Check correlations between variables: Pairgrid
+
+g=sb.PairGrid(df1)
+g.map_diag(plt.hist) #diagonal:histogramas
+g.map_offdiag(plt.scatter);#fora da diagonal scatterplots
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------OUTLIERS------------------#
+#Check distribution plots of variables (to see presence of outliers)
+import seaborn as sns
+sns.boxplot(x=dfOriginal["birthday"]) 
+sns.boxplot(x=dfOriginal["education"]) 
+sns.boxplot(x=dfOriginal["salary"]) 
+sns.boxplot(x=dfOriginal["livingArea"]) 
+sns.boxplot(x=dfOriginal["children"]) 
+sns.boxplot(x=dfOriginal["cmv"]) 
+sns.boxplot(x=dfOriginal["claims"]) 
+sns.boxplot(x=dfOriginal["lobMotor"]) 
+sns.boxplot(x=dfOriginal["lobHousehold"]) 
+sns.boxplot(x=dfOriginal["lobHealth"]) 
+sns.boxplot(x=dfOriginal["lobLife"])
+sns.boxplot(x=dfOriginal["lobWork"]) 
+
+#Create a dataframe with outliers
+q1=dfOriginal["salary"].quantile(0.25)
+q3=dfOriginal["salary"].quantile(0.75)
+iqr=q3-q1 #Interquartile range
+fence_low=q1-1.5*iqr
+fence_high=q3+1.5*iqr
+df_out=dfOriginal.loc[(dfOriginal["salary"] < fence_low) | (dfOriginal["salary"] > fence_high)]
+dfOriginal=dfOriginal.loc[(dfOriginal["salary"] > fence_low) & (dfOriginal["salary"] < fence_high)]
+
+def remove_outliers(df,df_out,column_name):  
+    q1=df[column_name].quantile(0.25)
+    q3=df[column_name].quantile(0.75)
+    iqr=q3-q1 #Interquartile range
+    fence_low=q1-1.5*iqr
+    fence_high=q3+1.5*iqr
+    df_out=df_out.append(df.loc[(df[column_name] < fence_low) | (df[column_name] > fence_high)])
+    df=df.loc[(df[column_name] > fence_low) & (df[column_name] < fence_high)]
+    return df_out,df
+
+
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"birthday")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"education")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"livingArea")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"children")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"cmv")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"claims")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"lobMotor")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"lobHousehold")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"lobHealth")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"lobLife")
+df_out,dfOriginal=remove_outliers(dfOriginal,df_out,"lobWork")
+
+dfOriginal.shape[0]
+df_out.shape[0] #1273 outliers in the df_out
+#-----------------TREAT NAN VALUES------------------#
+    #variable: firstPolicy
+    for i, index in dfOriginal.iterrows():
+            if str(dfOriginal.iloc[i,1])=='nan':
+                dfOriginal.iloc[i,1]=round(dfOriginal.iloc[:,1].mean(),0)
+    
+    #variable: birthday
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,2])=='nan':
+            dfOriginal.iloc[i,2]=round(dfOriginal.iloc[:,2].mean(),0)
+    
+    #variable: education - do the proportion and put the values
+    my_count=dfOriginal.groupby(by=["education"])["id"].count()#check class with more individuals
+    my_count
+    nrows=my_count[1]+my_count[2]+my_count[3]+my_count[0]
+    pbasic=round(my_count[0]/nrows*dfOriginal.isnull().sum()["education"],0)
+    pHS=round(my_count[1]/nrows*dfOriginal.isnull().sum()["education"],0)
+    pBSc_MSc=round(my_count[2]/nrows*dfOriginal.isnull().sum()["education"],0)
+    pPhd=round(my_count[3]/nrows*dfOriginal.isnull().sum()["education"],0)
+    
+    n=0
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,3])=='nan':
+            if n<2: 
+                dfOriginal.iloc[i,3]='1 - Basic'
+                n=n+1
+            elif n>=2 and n<8:
+                dfOriginal.iloc[i,3]='2 - High School'
+                n=n+1
+            elif n>=8 and n<16:
+                dfOriginal.iloc[i,3]='3 - BSc/MSc'
+                n=n+1
+            else:
+                dfOriginal.iloc[i,3]='4 - PhD'
+                n=n+1
+        
+    #variable: Salary: put mean
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,4])=='nan':
+            dfOriginal.iloc[i,4]=round(dfOriginal.iloc[:,4].mean(),0)
+    
+    #variable: Geographical area: see the most lived area:
+    my_countGA=dfOriginal.groupby(by=["livingArea"])["id"].count()#check class with more individuals
+    max_GA=my_countGA.max()
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,5])=='nan': dfOriginal.iloc[i,5]=max_GA
+    
+    #variable: children: calculate proportion?
+    my_count_child=dfOriginal.groupby(by=["children"])["id"].count()
+    nrows_child=my_count_child[1]+my_count_child[0]
+    pchild0=round(my_count_child[0]/nrows_child*dfOriginal.isnull().sum()["children"],0)
+    pchild1=round(my_count_child[1]/nrows_child*dfOriginal.isnull().sum()["children"],0)
+    pchild0
+    pchild1
+    n=0
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,6])=='nan':
+            if n<6: 
+                dfOriginal.iloc[i,6]=0
+                n=n+1
+            else:
+                dfOriginal.iloc[i,6]=1
+                n=n+1
+    
+    #variable: lobMotor, lobHealth, lobLife, lobWork: valor médio para que não tenham um grande impacto na análise
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,9])=='nan':
+            dfOriginal.iloc[i,9]=round(dfOriginal.iloc[:,9].mean(),0)
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,11])=='nan':
+            dfOriginal.iloc[i,11]=round(dfOriginal.iloc[:,11].mean(),0)
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,12])=='nan':
+            dfOriginal.iloc[i,12]=round(dfOriginal.iloc[:,12].mean(),0)
+    for i, index in dfOriginal.iterrows():
+        if str(dfOriginal.iloc[i,13])=='nan':
+            dfOriginal.iloc[i,13]=round(dfOriginal.iloc[:,13].mean(),0)
+
+
+
+#-----------------CHECK INCOHERENCES------------------#
+
+#if birthday is higher than First policy's year: 
+count_inc=0
+for i, index in dfOriginal.iterrows():
+    if dfOriginal.loc[]<dfOriginal.loc[]: count_inc+=1
+count_inc #there are 2000 rows that do not make sense. Individual's firstPolicy was before he even was born...
+
+dfOriginal.shape[0]
+#Create a data frame for the incoherent values
+df_inc=dfOriginal.loc[dfOriginal["birthday"]>dfOriginal["firstPolicy"]]
+df_inc.shape[0]
+dfOriginal=dfOriginal.loc[dfOriginal["birthday"]<=dfOriginal["firstPolicy"]]
+
+
+#Check incoherences in lobMotor variable
+    countmotor=0
+    for i, index in dfOriginal.iterrows():
+        if dfOriginal.loc[i,"lobMotor"]<0: countmotor+=1
+    countmotor #No incoherences
+
+#Check incoherences in lobHousehold variable:
+    counthousehold=0
+    for i, index in dfOriginal.iterrows():
+        if dfOriginal.loc[i,"lobHousehold"]<0: counthousehold+=1
+    counthousehold #there are 1007 observations with incoherences
+    #Add to df_inc and delete from dfOriginal
+    df_inc=df_inc.append(dfOriginal.loc[dfOriginal["lobHousehold"]<0])
+    dfOriginal=dfOriginal.loc[dfOriginal["lobHousehold"]>=0]
+
+#Check incoherences in lobHealth variable:
+    counthealth=0
+    for i, index in dfOriginal.iterrows():
+        if dfOriginal.loc[i,"lobHealth"]<0: counthealth+=1
+    counthealth #No incoherences
+
+#Check incoherences in lobLife variable
+    countlife=0
+    for i, index in dfOriginal.iterrows():
+        if dfOriginal.loc[i,"lobLife"]<0: countlife+=1
+    countlife #there are 462 observations with incoherences
+    #Add to df_inc and delete from dfOriginal
+    df_inc=df_inc.append(dfOriginal.loc[dfOriginal["lobLife"]<0])
+    dfOriginal=dfOriginal.loc[dfOriginal["lobLife"]>=0]
+
+#Check incoherences in lobWork variable
+    countWork=0
+    for i, index in dfOriginal.iterrows():
+        if dfOriginal.loc[i,'Premiums in LOB: Work Compensations']<0: countWork+=1
+    countWork #there are 550 observations with incoherences
+    #Add to df_inc and delete from dfOriginal
+    df_inc=df_inc.append(dfOriginal.loc[dfOriginal['Premiums in LOB: Work Compensations']<0])
+    dfOriginal=dfOriginal.loc[dfOriginal['Premiums in LOB: Work Compensations']>=0]
+
+#Check if column children is all 0's and 1's
+countchild=0
+for i, index in dfOriginal.iterrows():
+    if (dfOriginal.loc[i,'Has Children (Y=1)']!=0 and dfOriginal.loc[i,"children"]!=1): countchild+=1
+countchild
+
+#Check if all salaries are positive
+salary=0
+for i, index in dfOriginal.iterrows():
+    if dfOriginal.loc[i,"salary"]<0: salary+=1
+salary
+
+#Check if claims variable is >0
+    claims=0
+    for i, index in dfOriginal.iterrows():
+        if dfOriginal.loc[i,"claims"]<0: claims+=1
+    claims #No incoherences
+
+
+
+#-----------------NEW VARIABLES------------------#
+
+#Variable1: Sum of all LOB:
+dfOriginal['SumLOB']=dfOriginal["lobMotor"]+dfOriginal["lobHousehold"]+dfOriginal["lobHealth"]+dfOriginal["lobLife"]+dfOriginal["lobWork"]
+#Outra forma: dfOriginal['SumLOB'] = dfOriginal.apply(lambda row: row["lobMotor"] + row["lobHousehold"]+row["lobHealth"]+row["lobLife"]+row["lobWork"], axis=1)
+
+#Variable2: ratios of LOB:
+dfOriginal['motorRatio']=dfOriginal["lobMotor"]/dfOriginal['SumLOB']
+dfOriginal['householdRatio']=dfOriginal["lobHousehold"]/dfOriginal['SumLOB']
+dfOriginal['healthRatio']=dfOriginal["lobHealth"]/dfOriginal['SumLOB']
+dfOriginal['lifeRatio']=dfOriginal["lobLife"]/dfOriginal['SumLOB']
+dfOriginal['workCRatio']=dfOriginal["lobWork"]/dfOriginal['SumLOB']
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Scatter plots to see correlation between variables:
+fig, ax = plt.subplots(figsize=(16,8))
+ax.scatter(dfOriginal["salary"])
+ax.set_xlabel('Proportion of non-retail business acres per town')
+ax.set_ylabel('Full-value property-tax rate per $10,000')
+plt.show()
+
+
+
+
